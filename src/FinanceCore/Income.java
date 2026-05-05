1b5d9009b0cda3c34c0148a367e15235fc551ad4
bosha <beshorobert2004@gmail.com>
2026-05-06 00:50:23 +0300

    Move Files under src/ folder

---
+++ src/FinanceCore/Income.java
@@ -0,0 +1,30 @@
+package FinanceCore;
+
+//import java.sql.Date;
+import java.time.LocalDate;
+
+
+public class Income extends Transaction {
+    private String source;
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public Income(int transactionId, double amount, LocalDate date, String category, String source) {
+        super(transactionId, amount, date, category);
+        this.source = source;
+    }
+
+    void addIncome() {
+        if (validateAmount()) {
+            // Logic to add income to the system
+        } else {
+            // Handle invalid amount case
+        }
+    }
+}
