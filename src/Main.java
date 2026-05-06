f472af52c72e2a3f5569ce893d2f938262481615
bosha <beshorobert2004@gmail.com>
2026-05-06 18:35:19 +0300

    Transaction Edits

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        
+
         // Ensure database tables exist before doing anything else
         src.Infrastructure.DatabaseManager.getInstance().initializeDatabase();
 
